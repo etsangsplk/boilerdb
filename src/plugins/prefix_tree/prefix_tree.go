b857e9e383d7456fb541e07a47caafbc13314132
Dvir Volk <dvir@doat.com>
2012-11-17 16:06:07 +0200

    Added  a prefix tree plugin

---
+++ src/plugins/prefix_tree/prefix_tree.go
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dvirsky
+ * Date: 11/17/12
+ * Time: 1:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package prefix_tree
+
