5a8dc46432f2e1d065a42d7abae4fa48fdc20c52
Bill Mill <bill.mill@gmail.com>
2013-10-13 15:22:06 -0400

    improve config comments

--- src/config/config.go
+++ src/config/config.go
@@ -1,39 +1,30 @@
-/**
- * Created with IntelliJ IDEA.
- * User: dvirsky
- * Date: 11/16/12
- * Time: 4:13 AM
- * To change this template use File | Settings | File Templates.
- */
-
 package config
 
 import (
-//	goconf "code.google.com/p/goconf/conf"
-//	"camlistore.org/pkg/serverconfig"
-
-//"reflect"
 )
 
 var (
-	//where we'll save the dump files
+	//WORKING_DIRECTORY is where we'll save the dump files
 	WORKING_DIRECTORY = "/tmp"
 
-	//the buffer on the input and output channels of sessions
+	//IN_CHAN_BUFSIZE is the buffer on the input channel of sessions
 	IN_CHAN_BUFSIZE  int = 200
+
+	//OUT_CHAN_BUFSIZE is the buffer on the output channel of sessions
 	OUT_CHAN_BUFSIZE int = 100
 
-	//the buffer on a command sink's channel
+	//SINK_CHANNEL_SIZE is the buffer on a command sink's channel
 	SINK_CHANNEL_SIZE = 100
 
-	// save every N seconds if the database has changed
+	// BGSAVE_SECONDS tells the server to save every N seconds if the database
+	// has changed
 	// set to 0 for no saving
-	// setting the value too low will cause us to try the next time, no biggie
 	BGSAVE_SECONDS = 120
 
-	// The listening port of the database
+	// LISTEN_PORT defines the listening port of the database
 	LISTEN_PORT = 2000
 
+	// MAX_SYNC_RETRIES defines how many times to try resyncing the replication
 	MAX_SYNC_RETRIES = 5
 )
 
